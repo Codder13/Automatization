74f2c0c940b99b780fbc93b1e0de859bd1edc931
Denis <denisbolba@gmail.com>
2021-08-21 14:03:11 +0300

    done

--- File Management/build/fileOrganizer.bat
+++ File Management/build/fileOrganizer.bat
@@ -1,5 +1,5 @@
 @echo off
 
-py C:\Users\Denis\Programing\GitHub Programing\GithHub Automatization\File Management\main.py
+python C:\Users\Denis\Programing\GitHub Programing\GithHub Automatization\File Management\main.py
 
 @pause
